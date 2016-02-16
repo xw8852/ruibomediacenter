0fda7bd9ba0ee0de2b9deb2c9b9e86f9df1dd119
xw8852 <xw8852@126.com>
2016-02-16 22:46:23 +0800

    init project

---
+++ src/main/java/android/com/msx7/josn/ruibo_mediacenter/activity/LoginManager.java
@@ -0,0 +1,17 @@
+package android.com.msx7.josn.ruibo_mediacenter.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * 文件名: LoginManager
+ * 描  述:
+ * 作  者：Josn
+ * 时  间：2016/2/16
+ */
+public class LoginManager extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ src/main/java/android/com/msx7/josn/ruibo_mediacenter/activity/MainActivity.java
@@ -0,0 +1,20 @@
+package android.com.msx7.josn.ruibo_mediacenter.activity;
+
+import android.app.Activity;
+import android.com.msx7.josn.ruibo_mediacenter.R;
+import android.os.Bundle;
+
+/**
+ * 文件名: MainActivity
+ * 描  述:
+ * 作  者：Josn
+ * 时  间：2016/2/16
+ */
+public class MainActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
